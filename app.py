4278e5c42aa0fc91777ea65fb45c6b1ead71a8d5
smallken99 <smallken@gmail.com>
2019-01-29 22:53:46 +0800

    put on githut

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return "<h1>Hello, World!</h1>"
+    title = "Flask Web App"
+    paras = [
+        'section 1',
+        'section 2',
+        'stction 3'
+    ]    
+    return render_template("index.html", title=title,data=paras)
 
 @app.route('/user/<name>')
 def user(name):
