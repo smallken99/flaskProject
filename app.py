ab2bb4763acb68312e2d318aedd41dadb4b692db
smallken99 <smallken@gmail.com>
2019-01-30 21:41:03 +0800

    設定sqlite資料庫 Flask-SQLAlchemy 建立表單 Flask-WTF

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegisterForm
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
@@ -14,6 +20,13 @@ def index():
         'stction 3'
     ]    
     return render_template("index.html", title=title,data=paras)
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+    return render_template("register.html",form=form)
 
 @app.route('/user/<name>')
 def user(name):
